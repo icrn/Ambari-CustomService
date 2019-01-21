cf71fcdaff8f4410d2803af665d64f011b45f02f
icrn <294524708@qq.com>
2019-01-21 19:21:49 +0800

    修改logstash check status错误

--- src/LOGSTASH/package/scripts/master.py
+++ src/LOGSTASH/package/scripts/master.py
@@ -25,7 +25,7 @@ import signal
 import time
 from resource_management import *
 from logstash import logstash
-
+from resource_management.libraries.functions.check_process_status import check_process_status
 
 class Master(Script):
     def install(self, env):
@@ -34,8 +34,6 @@ class Master(Script):
         self.install_packages(env)
         logstash()
         self.configure(env)
-        reload_cmd = format("systemctl daemon-reload")
-        Execute(reload_cmd)
 
     def configure(self, env):
         import params
@@ -44,6 +42,19 @@ class Master(Script):
                group=params.logstash_group,
                content=Template("logstash.conf.j2")
         )
+        File(format("/etc/sysconfig/logstash"),
+               owner="root",
+               group="root",
+               content=Template("logstash.j2")
+        )
+        File(format("/etc/systemd/system/logstash.service"),
+               owner="root",
+               group="root",
+               content=Template("logstash.service.j2")
+        )
+        reload_cmd = format("systemctl daemon-reload")
+        Execute(reload_cmd)
+
         Execute('echo "Configuration complete"')
 
     def stop(self, env):
@@ -51,15 +62,18 @@ class Master(Script):
         Execute(stop_cmd)
         Execute('echo "stop complete"')
 
-    def start(self, env):
-        stop_cmd = format("service logstash start")
-        Execute(stop_cmd)
-        Execute('echo "start complete"')
-
     def status(self, env):
-        stop_cmd = format("service logstash status")
-        print "check status"
-        Execute(stop_cmd)
+        print "check start"
+        os.system(r"ps -ef | grep org.logstash.Logstash | grep -v grep | awk '{print $2}' > /var/run/logstash.pid")
+
+        import status_params
+        env.set_params(status_params)
+        check_process_status(status_params.logstash_pid_file)
+
+    def start(self, env):
+        start_cmd = format("service logstash start")
+        print "check start"
+        Execute(start_cmd)
 
 if __name__ == "__main__":
     Master().execute()
